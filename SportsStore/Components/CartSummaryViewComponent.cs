486892e34d25f895e6c7079a9ff81caa73641f04
Auriel <amadera@protonmail.com>
2021-01-01 20:11:21 +0300

    sync

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components {
+    public class CartSummaryViewComponent: ViewComponent {
+        private Cart CartService { get; set; }
+        
+        public CartSummaryViewComponent(Cart cartService) => CartService = cartService;
+        
+        public IViewComponentResult Invoke() => View(CartService);
+    }
+}
